2e3a272bd1ec483627458772a63c7224fbea3fe2
barneslardo <37343575+barneslardo@users.noreply.github.com>
2018-11-19 17:50:44 -0800

    add updated files

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ var express = require("express"),
 
 app.set("view engine", "ejs");
 
+// This is a note!
 app.get("/", (req, res) =>{
     res.send({hi: 'there'});
     console.log("main hit");
 });
 
-app.listen(5000);
+app.get("/annie", (req, res) =>{
+  res.render("annie");
+  console.log("Annie is HOT!!!");
+});
+
+app.get("*", (req, res) =>{
+  res.render("catch");
+  console.log("catch hit");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 console.log("Server is running");
